7480f5cb45fd2c1c73262deceb749287c99199dd
hemjeet <146974266+hemjeet@users.noreply.github.com>
2026-05-25 21:06:08 +0530

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbd6qrcj31rXCxAN1bfR97_Yav8T3X0ME",
+  authDomain: "rent-manager-2cce8.firebaseapp.com",
+  projectId: "rent-manager-2cce8",
+  storageBucket: "rent-manager-2cce8.firebasestorage.app",
+  messagingSenderId: "557604389175",
+  appId: "1:557604389175:web:3436efc854999311db9ab2",
+  measurementId: "G-8GSKGVB6D1"
+};
